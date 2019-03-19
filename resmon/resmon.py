30796c2b939315ddaa0752b2c99a50d4290f5352
NINXC <16856738+NIXC@users.noreply.github.com>
2019-03-18 22:48:49 -0600

    gitignore hacking

--- resmon/resmon.py
+++ resmon/resmon.py
@@ -8,13 +8,19 @@ try:
 except ImportError:
     psutilAvailable = False
 
-BaseCog = getattr(commands, "Cog", object)
 
-class resmon(BaseCog):
+class resmon(commands.Cog):
     """Record system information about the server running this instance"""
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.group(name="resmon")
     async def resmon(self,ctx):
         """Record system information about the server running this instance"""
         pass
+
+    
+
+if __name__ == "__main__":
+    print("hi")
+    print(psutilAvailable)
