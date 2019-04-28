3c219eb59241ab9bbd7e0a8a6abc423b2a52e718
NINXC <16856738+NIXC@users.noreply.github.com>
2019-04-28 13:15:23 -0600

    kill rip
    rip was bad and I feel bad

--- rip/__init__.py
+++
@@ -1,7 +0,0 @@
-from rip.rip import rip, pillowAvailable
-
-def setup(bot):
-    if pillowAvailable:
-        bot.add_cog(rip(bot))
-    else:
-        raise RuntimeError("Need to run 'pip install pillow'. You may also need to install libjpeg-dev")
