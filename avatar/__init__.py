9bc38a919d9b904bff9bac0315d62f4fa5e50935
NINXC <16856738+NIXC@users.noreply.github.com>
2019-07-27 13:59:55 -0600

    avatar finder

---
+++ avatar/__init__.py
@@ -0,0 +1,4 @@
+from avatar.avatar import avatar
+
+def setup(bot):
+    bot.add_cog(avatar(bot))
---
+++ avatar/avatar.py
@@ -0,0 +1,31 @@
+#Credits to Yukirin#0048 for the code I based this off of.
+from redbot.core import commands
+
+class avatar(commands.Cog):
+    """Get user's avatar URL.""" 
+    @commands.command()
+    async def avatar(self, ctx, target):
+        """Gets a user's avatar. Attempts to search all guilds this bot is in. Accepts only user ID"""
+        if target is None or type(target) != type(26):
+            user = ctx.author
+        else:
+            for guild in self.bot.guilds:
+                user = guild.get_member(target)
+                if user is not None:
+                    break
+
+        if user is not None:
+            if user.is_avatar_animated():
+                url = user.avatar_url_as(format="gif")
+            else:
+                url = user.avatar_url_as(static_format="png")
+            await ctx.send("{}'s Avatar URL : {}".format(user.name, url))
+        else:
+            await ctx.send(f"Unable to find user")
+        
+
+
+        
+        
+
+
